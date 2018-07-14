04eb459cac6d42c8ab07a872cec3c0d0e82fde88
Rishabh Saxena <rishabh-999@hotmail.com>
2018-07-14 12:22:33 -0400

    WP: trying newing Mongolab databse

--- src/main/java/com/levelupquest/controllers/HomeController.java
+++ src/main/java/com/levelupquest/controllers/HomeController.java
@@ -1,3 +1,4 @@
+
 package com.levelupquest.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
--- src/main/java/com/levelupquest/repositories/CustomerRepository.java
+++ src/main/java/com/levelupquest/repositories/CustomerRepository.java
@@ -2,10 +2,11 @@ package com.levelupquest.repositories;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.levelupquest.entities.Customer;
 
 @Repository
-public interface CustomerRepository extends CrudRepository<Customer, Integer> {
+public interface CustomerRepository extends  MongoRepository<Customer, Integer> {
 
 }
