00d2629daff3110777852e8fd82ac2f9386a997d
Rishabh Saxena <rishabh-999@hotmail.com>
2018-07-17 16:55:45 -0400

    made components

--- src/main/webapp/frontend/src/app/app.module.ts
+++ src/main/webapp/frontend/src/app/app.module.ts
@@ -5,11 +5,19 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { DataServiceService } from './data-service.service';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NotificationComponent } from './notification/notification.component';
+import { FormComponent } from './form/form.component';
+import { PaymentComponent } from './payment/payment.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    DashboardComponent,
+    NotificationComponent,
+    FormComponent,
+    PaymentComponent
   ],
   imports: [
     BrowserModule,
