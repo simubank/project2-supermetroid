f681fd1fdb2c9438a4848823d7b6f18cfc570dd2
Krishan <billak@sheridancollege.ca>
2018-07-19 19:56:01 -0400

    Adding Notification Request Mappings

--- src/main/java/com/levelupquest/controllers/HomeController.java
+++ src/main/java/com/levelupquest/controllers/HomeController.java
@@ -33,8 +33,9 @@ public class HomeController {
 
 	@GetMapping(value = "/customer/{id}")
 	public Customer getCustomerById(@PathVariable String id) {
-		return this.customerService.getCustomerByApiId(id).isPresent() 
-				? this.customerService.getCustomerByApiId(id).get() : null;
+		return this.customerService.getCustomerByApiId(id).isPresent()
+				? this.customerService.getCustomerByApiId(id).get()
+				: null;
 	}
 
 	@PostMapping(value = "/customer")
@@ -42,25 +43,33 @@ public class HomeController {
 		return this.customerService.save(customer);
 	}
 
-
 	@PutMapping(value = "/customer")
 	public Customer update(@RequestBody Customer customer) {
 
 		return this.customerService.update(customer);
 	}
 
-	@GetMapping(value="/test")
+	@GetMapping(value = "/test")
 	public Customer test() {
 		String id = "63363738-f374-4490-83d4-be9bfba401f1_e2ba9727-a181-48f6-a1bc-0abf5ce173a";
 		Customer c = this.customerService.getCustomerByApiId(id).get();
-		c.getAllowanceAccount().setStartDate( LocalDate.now());
+		c.getAllowanceAccount().setStartDate(LocalDate.now());
 		this.customerService.save(c);
-		
+
 		return c;
 	}
-	
-	@GetMapping(value="/notifications/{id}")
-	public List<Notification> getNotifications(@PathVariable String id){
+
+	@GetMapping(value = "/notifications/{id}")
+	public List<Notification> getNotifications(@PathVariable String id) {
 		return this.customerService.getCustomerByApiId(id).get().getNotifications();
+	}
+
+	@PostMapping(value = "/notifications/{id}")
+	public List<Notification> postNotificatiton(@RequestBody String notificationText, @PathVariable String id) {
+		Notification notification = new Notification(notificationText);
+		Customer customer = this.customerService.getCustomerByApiId(id).get();
+		customer.getNotifications().add(notification);
+		this.customerService.save(customer);
+		return customer.getNotifications();
 	}
 }
