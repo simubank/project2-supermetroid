6a6bbbcafd0c2abce045456556d08b8bb95ef1a4
Heung Park <hpark327@uwo.ca>
2018-07-21 13:05:07 -0400

    commit1

--- src/main/webapp/frontend/src/app/app.module.ts
+++ src/main/webapp/frontend/src/app/app.module.ts
@@ -2,14 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { DataServiceService } from './data-service.service';
+import { TermsComponent } from './terms/terms.component';
+import { SetupAmountComponent } from './setup-amount/setup-amount.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    TermsComponent,
+    SetupAmountComponent
   ],
   imports: [
     BrowserModule,
