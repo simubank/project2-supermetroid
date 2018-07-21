faeaf78512d8b67acc593411e34db1c9938550d7
Krishan <billak@sheridancollege.ca>
2018-07-21 13:35:17 -0400

    Adding input()

--- src/main/webapp/frontend/src/app/dashboard/dashboard.component.ts
+++ src/main/webapp/frontend/src/app/dashboard/dashboard.component.ts
@@ -1,27 +1,28 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import {LOCAL_STORAGE, WebStorageService} from 'angular-webstorage-service';
+import { Customer } from '../model/Customer';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
+
+
 export class DashboardComponent implements OnInit {
 
+  public customer: Customer = new Customer();
   constructor(@Inject(LOCAL_STORAGE) private storage: WebStorageService) { }
 
   ngOnInit() {
-    this.storage.set("1",232);
+
   }
 
 
-  saveInLocal(key, val): void {
-    this.storage.set("1",232); 
-    }
+
 
 }
 
 
 
 
-     
--- src/main/webapp/frontend/src/app/payment/payment.component.ts
+++ src/main/webapp/frontend/src/app/payment/payment.component.ts
@@ -17,7 +17,6 @@ export class PaymentComponent implements OnInit {
   constructor() {
   }
   ngOnInit() {
-
   }
 
   getTransactions() {
