4544916a2ced8f8d4b7d8898dbcdf91b45f94978
Heung Park <hpark327@uwo.ca>
2018-07-22 18:24:23 -0400

    Settings Page UI Update

--- src/main/webapp/frontend/src/app/setup-amount/setup-amount.component.ts
+++ src/main/webapp/frontend/src/app/setup-amount/setup-amount.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { DataServiceService } from '../data-service.service';
 
-
 @Component({
   selector: 'app-setup-amount',
   templateUrl: './setup-amount.component.html',
@@ -11,13 +10,29 @@ export class SetupAmountComponent implements OnInit {
 
 
   public customer: any;
+  public hasSavings: boolean;
+  public timeFrame: Array<String>;
+  public sliderVal: number;
+  public account: string;
   constructor(private dataService: DataServiceService) {
 
   }
 
   ngOnInit() {
+    this.account = "Chequeing";
+    this.hasSavings = true;
+    this.timeFrame = ['Daily', 'Weekly', 'Bi-Weekly', 'Monthly' ];
     this.dataService.currentMessage.subscribe(
       (customer) => this.customer = customer);
   }
+
+  onClickChequeing() {
+    this.account = 'Chequeing';
+  }
+
+  onClickSavings() {
+    this.account = 'Savings';
+  }
+  
 
 }
