79482deaa9b0a79b42268b0ec64229f0104ff4e6
Heung Park <hpark327@uwo.ca>
2018-07-21 22:47:52 -0400

    Finished settings, navbar, notifications, setup

--- src/main/webapp/frontend/src/app/app-routing.module.ts
+++ src/main/webapp/frontend/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { TermsComponent } from './terms/terms.component';
 import { SetupAmountComponent } from './setup-amount/setup-amount.component';
+import { NotificationComponent } from './notification/notification.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent},
   { path: 'settings', component: SetupAmountComponent},
-  { path: 'terms', component: TermsComponent}
+  { path: 'terms', component: TermsComponent},
+  { path: 'notification', component: NotificationComponent}
 ];
 
 @NgModule({
