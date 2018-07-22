2c35ae15db76f0fd2f94bf6cbeeb9153cb0035d6
Rishabh Saxena <rishabh-999@hotmail.com>
2018-07-22 12:38:49 -0400

    new stuff on dashboard

--- src/main/webapp/frontend/src/app/dashboard/dashboard.component.ts
+++ src/main/webapp/frontend/src/app/dashboard/dashboard.component.ts
@@ -13,12 +13,25 @@ export class DashboardComponent implements OnInit {
 
   @Output() valueChange = new EventEmitter() ;
   public customer: any = '';
+  private currentBalance:number; 
+  private timeLeft:string;
+  private totalBalance:number; 
+  private points:number;
+  private transaction: any[]; 
+  private icon:number; 
+
+
   private customerIds: string[] = [
       '63363738-f374-4490-83d4-be9bfba401f1_6c8434d3-9d00-45d9-83d6-5c87cc97cdd8',
       '63363738-f374-4490-83d4-be9bfba401f1_85a09159-bda3-426a-bcd3-00532807d1df',
       '63363738-f374-4490-83d4-be9bfba401f1_e2ba9727-a181-48f6-a1bc-0abf5ce173a'
   ];
   constructor(private dataService: DataServiceService) {
+    this.currentBalance=99; 
+    this.totalBalance=100; 
+    this.timeLeft="2Days!"; 
+    this.points=0;
+    this.icon=1; 
    }
 
   ngOnInit() {
