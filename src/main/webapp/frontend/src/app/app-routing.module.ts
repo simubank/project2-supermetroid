6eab5b9281d39403350989af5e93947b25997de0
matthew-jay-wong <matthew.jay.wong@gmail.com>
2018-07-22 13:15:57 -0400

    notifications bell needs to light up after new notifications come in

--- src/main/webapp/frontend/src/app/app-routing.module.ts
+++ src/main/webapp/frontend/src/app/app-routing.module.ts
@@ -1,4 +1,3 @@
-import { NotificationComponent } from './notification/notification.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -11,11 +10,7 @@ const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent},
   { path: 'settings', component: SetupAmountComponent},
   { path: 'terms', component: TermsComponent},
-<<<<<<< HEAD
   { path: 'notifications', component: NotificationComponent}
-=======
-  { path: 'notification', component: NotificationComponent}
->>>>>>> 79482deaa9b0a79b42268b0ec64229f0104ff4e6
 ];
 
 @NgModule({
